5cecf9dfcf7e8a470ed24a02fb6e3a4fc80f9e8c
raskour <rasmeetkour735@gmail.com>
2023-09-17 19:33:53 +1000

    add cors

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const restaurants = require('./mock-data/restaurants') // importing data from restaurants.js
+const express = require("express");
+const cors = require("cors");
+const restaurants = require("./mock-data/restaurants"); // importing data from restaurants.js
 
-const app = express()
-const port = 3000
+const app = express();
+app.use(cors());
+const port = 3000;
 
-app.get('/api/restaurants/', (req, res) => {
+app.get("/api/restaurants/", (req, res) => {
   res.json(restaurants);
-})
+});
 
 // get single restaurant by Id
-app.get('/api/restaurants/:resId', (req, res) => {
-     const id = req.params.resId; 
-     const restaurant = restaurants.find((res) => res.data.data.id === id);
-     res.json(restaurant);
-
-  })
+app.get("/api/restaurants/:resId", (req, res) => {
+  const id = req.params.resId;
+  const restaurant = restaurants.find((res) => res.id === id);
+  res.json(restaurant);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+  console.log(`Example app listening on port ${port}`);
+});
